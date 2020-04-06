d0377d9db10a1816e43817bc841f385697a98916
Jonas Verhofsté <jonas.verhofste@ugent.be>
2020-04-06 20:32:40 +0200

    Add csvserver_cachepolicy_binding type and provider

---
+++ lib/puppet/provider/netscaler_csvserver_cachepolicy_binding/rest.rb
@@ -0,0 +1,47 @@
+require_relative '../../../puppet/provider/netscaler_binding'
+
+Puppet::Type.type(:netscaler_csvserver_cachepolicy_binding).provide(:rest, parent: Puppet::Provider::NetscalerBinding) do
+  def netscaler_api_type
+    'csvserver_cachepolicy_binding'
+  end
+
+  def self.instances
+    instances = []
+    csvservers = Puppet::Provider::Netscaler.call('/config/csvserver')
+    return [] if csvservers.nil?
+
+    csvservers.each do |csvserver|
+      binds = Puppet::Provider::Netscaler.call("/config/csvserver_cachepolicy_binding/#{csvserver['name']}") || []
+      binds.each do |bind|
+        instances << new(ensure: :present,
+                         name: "#{bind['name']}/#{bind['policyname']}",
+                         priority: bind['priority'],
+                         bindpoint: bind['bindpoint'],
+                         goto_expression: bind['gotopriorityexpression'],
+                         label_name: bind['labelname'])
+      end
+    end
+
+    instances
+  end
+
+  mk_resource_methods
+
+  def property_to_rest_mapping
+    {
+      goto_expression: :gotopriorityexpression,
+      label_name: :labelname,
+    }
+  end
+
+  def per_provider_munge(message)
+    message[:name], message[:policyname] = message[:name].split('/')
+
+    if message[:label_name]
+      message[:labeltype] = 'policylabel'
+      message[:invoke] = 'true'
+    end
+
+    message
+  end
+end
---
+++ lib/puppet/type/netscaler_csvserver_cachepolicy_binding.rb
@@ -0,0 +1,63 @@
+require_relative('../../puppet/property/netscaler_truthy')
+
+Puppet::Type.newtype(:netscaler_csvserver_cachepolicy_binding) do
+  @doc = 'Manage a binding between a content switching vserver and a cache policy.'
+
+  apply_to_device
+  ensurable
+
+  newparam(:name, namevar: true) do
+    desc 'csvserver_name/policy_name'
+  end
+
+  newproperty(:priority) do
+    desc "The priority of the policy binding.
+
+Min = 1
+Max = 2147483647"
+    newvalues(%r{^\d+$})
+    munge do |value|
+      Integer(value)
+    end
+  end
+
+  newproperty(:bindpoint) do
+    desc 'The bindpoint to which the policy is bound.Possible values = REQUEST, RESPONSE'
+  end
+
+  newproperty(:goto_expression) do
+    desc 'Expression specifying the priority of the next policy which will get evaluated if the current policy rule evaluates to TRUE'
+  end
+
+  newproperty(:label_name) do
+    desc 'Label of policy to invoke if the bound policy evaluates to true.'
+  end
+
+  autorequire(:netscaler_csvserver) do
+    if !self.deleting?
+      self[:name].split('/')[0]
+    end
+  end
+
+  autobefore(:netscaler_csvserver) do
+    if self.deleting?
+      self[:name].split('/')[0]
+    end
+  end
+
+  autorequire(:netscaler_cachepolicy) do
+    if !self.deleting?
+      self[:name].split('/')[1]
+    end
+  end
+
+  autobefore(:netscaler_cachepolicy) do
+    if self.deleting?
+      self[:name].split('/')[1]
+    end
+  end
+
+  autorequire(:netscaler_cachepolicylabel) do
+    self[:label_name]
+  end
+end
